fe8c6f5842bb11cc0b8e109ecaead1f6ec750d1b
Josh Feierman <josh@sqljosh.com>
2021-10-10 16:49:56 -0400

    Added session package

---
+++ session/session_test.go
@@ -0,0 +1,71 @@
+package session
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	//"github.com/yardbirdsax/goawsssm/client"
+	"github.com/yardbirdsax/goawsssm/mock"
+)
+
+
+func TestStart(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockClient := mock.NewMockSSMClient(ctrl)
+	instanceID := "i-abcd123456"
+	sessionID := "abcd123456"
+	streamURL := "http://127.0.0.1"
+	tokenValue := "xyz0123456"
+	documentName := "document"
+	parameters := map[string][]string{
+		"hello": {"world"},
+	}
+	maxRetries := 5
+	retryWaitInterval := 5*time.Second
+	ctx := context.TODO()
+	startSesionInput := &StartSessionInput{
+		InstanceID: instanceID,
+		MaxRetries: maxRetries,
+		RetryWaitInterval: retryWaitInterval,
+		DocumentName: documentName,
+		Parameters: parameters,
+	}
+	startSessionInputClient := &ssm.StartSessionInput{
+		Target: &instanceID,
+		DocumentName: &documentName,
+		Parameters: parameters,
+	}
+	callCounter := 0
+	mockClient.EXPECT().StartSession(ctx, startSessionInputClient).
+		Times(maxRetries).
+		DoAndReturn(
+		func (ctx context.Context, input *ssm.StartSessionInput) (output *ssm.StartSessionOutput, err error) {
+			callCounter ++
+			if callCounter < 5 {
+				output = nil
+				err = fmt.Errorf("I'm an error")
+			} else {
+				output = &ssm.StartSessionOutput{
+					SessionId: &sessionID,
+					StreamUrl: &streamURL,
+					TokenValue: &tokenValue,
+				}
+			}
+			return
+		},
+	)
+
+	output, err := Start(ctx, mockClient, *startSesionInput)
+
+	ctrl.Finish()
+	assert.NotNil(t, output)
+	assert.Nil(t, err, "Start function returned an error unexpectedly.")
+	assert.Equal(t, sessionID, *(output.SessionId))
+	assert.Equal(t, streamURL, *(output.StreamUrl))
+	assert.Equal(t, tokenValue, *(output.TokenValue))
+}
